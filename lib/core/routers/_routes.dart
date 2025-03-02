1b574033768d2553c93d8715b6bcd2a93706e8af
codeZeus-Ojonaw <Emrys_02@outlook.com>
2025-03-03 00:06:50 +0100

    fix: fix

--- lib/core/routers/_routes.dart
+++ lib/core/routers/_routes.dart
@@ -1,7 +1,7 @@
 import 'package:defifundr_mobile/core/routers/routes_constant.dart';
 import 'package:defifundr_mobile/feature/create_password/create_password_screen.dart';
 import 'package:defifundr_mobile/features/authentication/presentation/complete_kyc/complete_kyc.dart';
-import 'package:defifundr_mobile/features/home_page/widgets/home_page.dart';
+import 'package:defifundr_mobile/features/contract/presentation/view/compliance-page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
