c03eb9346af5e040fb47fd6586bd62176902fd48
codeZeus-Ojonaw <diochuks65@gmail.com>
2025-03-26 12:26:12 +0100

    fix: remove imports

--- lib/core/routers/routers.dart
+++ lib/core/routers/routers.dart
@@ -2,7 +2,6 @@ import 'package:defifundr_mobile/core/routers/routes_constant.dart';
 import 'package:defifundr_mobile/screens/auth_screens/contract_overview/view/contract_overview.dart';
 import 'package:defifundr_mobile/screens/auth_screens/create_password/create_password_screen.dart';
 import 'package:defifundr_mobile/screens/auth_screens/get_started/view/get_started.dart';
-import 'package:defifundr_mobile/screens/home/contract/presentation/view/compliance_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
