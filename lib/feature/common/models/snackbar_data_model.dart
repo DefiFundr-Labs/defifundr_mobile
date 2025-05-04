c429fbb69d1df356e1a19b7e5f3a3c810d4a5298
Emrys <Emrys_02@outlook.com>
2025-05-04 04:25:04 +0100

    refactor(snackbar)!: remove SnackbarService and related models/widgets
    Signed-off-by: Emrys <Emrys_02@outlook.com>

--- lib/feature/common/models/snackbar_data_model.dart
+++
@@ -1,6 +0,0 @@
-class SnackbarData {
-  final String title;
-  final String message;
-  final bool isError;
-  const SnackbarData({required this.title, required this.message, required this.isError});
-}
