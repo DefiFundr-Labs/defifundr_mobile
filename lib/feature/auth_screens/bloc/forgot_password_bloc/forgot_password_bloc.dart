5d888b8a5fb244d2e77394b4742af8400e047f66
Emrys <Emrys_02@outlook.com>
2025-05-04 01:51:19 +0100

    refactor(forgot_password_bloc): streamline event handlers and improve code organization
    Signed-off-by: Emrys <Emrys_02@outlook.com>

--- lib/feature/auth_screens/bloc/forgot_password_bloc/forgot_password_bloc.dart
+++ lib/feature/auth_screens/bloc/forgot_password_bloc/forgot_password_bloc.dart
@@ -13,69 +13,86 @@ class ForgotPasswordBloc extends Bloc<ForgotPasswordEvent, ForgotPasswordState> 
   final _snackbarService = SnackbarService();
 
   ForgotPasswordBloc() : super(ForgotPasswordInitial()) {
-    on<SubmitEmail>((event, emit) {
-      if (!EmailValidator.validate(event.email)) {
-        emit(ForgotPasswordError("Invalid email format"));
-        return;
-      }
-      emit(ForgotPasswordSuccess("A reset link has been sent to ${event.email}"));
-    });
+    on<SubmitEmail>(_submitEmailHandler);
 
     on<EnterPasswordString>(
-      (event, emit) async {
-        // Adding a delay to prevent overloading the UI with state changes
-        // This is a simple way to debounce the event and prevent rapid state changes
-        await Future.delayed(Duration(milliseconds: 300));
-        emit(
-          ForgotPasswordInitial(
-            newPasswordState:
-                (state.newPasswordState ?? NewPasswordState(password: state.newPasswordState?.password ?? '')).copyWith(password: event.passwordString),
-          ),
-        );
-      },
+      _enterPasswordStringHandler,
       // Use the `restartable` transformer to prevent multiple rapid state changes
       // that can occur when the user rapidly toggles the password visibility
       // This will ensure that only the last event is processed after the delay
       transformer: restartable(),
     );
     on<EnterConfirmPasswordString>(
-      (event, emit) async {
-        // Adding a delay to prevent overloading the UI with state changes
-        // This is a simple way to debounce the event and prevent rapid state changes
-        await Future.delayed(Duration(milliseconds: 300));
-        emit(
-          ForgotPasswordInitial(
-            newPasswordState:
-                (state.newPasswordState ?? NewPasswordState(password: state.newPasswordState?.password ?? '')).copyWith(confirmPassword: event.passwordString),
-          ),
-        );
-      },
+      _enterConfirmPasswordStringHandler,
       // Use the `restartable` transformer to prevent multiple rapid state changes
       // that can occur when the user rapidly toggles the password visibility
       // This will ensure that only the last event is processed after the delay
       transformer: restartable(),
     );
-    on<TogglePasswordVisibility>((event, emit) {
-      emit(ForgotPasswordInitial(
-          newPasswordState: (state.newPasswordState ?? NewPasswordState(password: state.newPasswordState?.password ?? ''))
-              .copyWith(showPassword: !(state.newPasswordState?.hidePassword ?? false))));
-    });
-    on<ToggleConfirmPasswordVisibility>(
-      (event, emit) {
-        emit(
-          ForgotPasswordInitial(
-            newPasswordState: (state.newPasswordState ?? NewPasswordState(password: state.newPasswordState?.password ?? '')).copyWith(
-              showConfirmPassword: !(state.newPasswordState?.hideConfirmPassword ?? false),
-            ),
-          ),
-        );
-      },
+    on<TogglePasswordVisibility>(_togglePasswordVisibilityHandler);
+    on<ToggleConfirmPasswordVisibility>(_toggleConfirmPasswordVisibilityHandler);
+    on<ResendOtpEvent>(_resendOtpHandler);
+    on<VerifyOtp>(_verifyOtpHandler);
+  }
+
+  void _submitEmailHandler(SubmitEmail event, Emitter<ForgotPasswordState> emit) {
+    if (!EmailValidator.validate(event.email)) {
+      emit(ForgotPasswordError("Invalid email format"));
+      return;
+    }
+    emit(ForgotPasswordSuccess("A reset link has been sent to ${event.email}"));
+  }
+
+  Future<void> _enterPasswordStringHandler(EnterPasswordString event, Emitter<ForgotPasswordState> emit) async {
+    // Adding a delay to prevent overloading the UI with state changes
+    // This is a simple way to debounce the event and prevent rapid state changes
+    await Future.delayed(Duration(milliseconds: 300));
+    emit(
+      ForgotPasswordInitial(
+        newPasswordState:
+            (state.newPasswordState ?? NewPasswordState(password: state.newPasswordState?.password ?? '')).copyWith(password: event.passwordString),
+      ),
     );
-    on<ResendOtpEvent>((event, emit) {
-      _snackbarService.showSuccessSnackbar(title: AppTexts.otpCodeResent, message: AppTexts.otpCodeResentDesc);
-    });
-    on<VerifyOtp>((event, emit) {
-      _snackbarService.showErrorSnackbar(title: AppTexts.invalidOTPCode, message: AppTexts.invalidOTPCodeDesc);
-    });
+  }
+
+  Future<void> _enterConfirmPasswordStringHandler(EnterConfirmPasswordString event, Emitter<ForgotPasswordState> emit) async {
+    // Adding a delay to prevent overloading the UI with state changes
+    // This is a simple way to debounce the event and prevent rapid state changes
+    await Future.delayed(Duration(milliseconds: 300));
+    emit(
+      ForgotPasswordInitial(
+        newPasswordState:
+            (state.newPasswordState ?? NewPasswordState(password: state.newPasswordState?.password ?? '')).copyWith(confirmPassword: event.passwordString),
+      ),
+    );
+  }
+
+  void _togglePasswordVisibilityHandler(TogglePasswordVisibility event, Emitter<ForgotPasswordState> emit) {
+    emit(
+      ForgotPasswordInitial(
+        newPasswordState: (state.newPasswordState ?? NewPasswordState(password: state.newPasswordState?.password ?? '')).copyWith(
+          showPassword: !(state.newPasswordState?.hidePassword ?? false),
+        ),
+      ),
+    );
+  }
+
+  void _toggleConfirmPasswordVisibilityHandler(ToggleConfirmPasswordVisibility event, Emitter<ForgotPasswordState> emit) {
+    emit(
+      ForgotPasswordInitial(
+        newPasswordState: (state.newPasswordState ?? NewPasswordState(password: state.newPasswordState?.password ?? '')).copyWith(
+          showConfirmPassword: !(state.newPasswordState?.hideConfirmPassword ?? false),
+        ),
+      ),
+    );
+  }
+
+  void _resendOtpHandler(ResendOtpEvent event, Emitter<ForgotPasswordState> emit) {
+    _snackbarService.showSuccessSnackbar(title: AppTexts.otpCodeResent, message: AppTexts.otpCodeResentDesc);
+  }
+
+  void _verifyOtpHandler(VerifyOtp event, Emitter<ForgotPasswordState> emit) {
+    _snackbarService.showErrorSnackbar(title: AppTexts.invalidOTPCode, message: AppTexts.invalidOTPCodeDesc);
+    emit(ForgotPasswordError(AppTexts.invalidOTPCode));
   }
 }
