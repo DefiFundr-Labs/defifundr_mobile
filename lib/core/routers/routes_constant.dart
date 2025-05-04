6865a4a8719977fc64d59f9f8cdea58aeba8307b
Lewechi <lewechigodsfavour@gmail.com>
2025-05-04 12:34:39 +0100

    feat: login screen implementation

--- lib/core/routers/routes_constant.dart
+++ lib/core/routers/routes_constant.dart
@@ -1,11 +1,11 @@
 class RouteConstants {
   static String initial = '/';
   static String login = 'login';
-  static String verifyOtp = 'verify-otp';
   static String newPassword = 'new-password';
   static String personalDetails = 'personalDetails';
   static String countrySelection = 'countrySelection';
   static String dialCodeSelection = 'dialCodeSelection';
   static String addressDetails = 'addressDetails';
   static String profileCreated = 'profileCreated';
+  static String accountType = 'accountType';
 }
