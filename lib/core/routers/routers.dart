70336ce91a61036f99a54dd589f337a44ac2a180
Emrys <Emrys_02@outlook.com>
2025-05-18 17:15:43 +0100

    fix(router): update routing to use appropriate screens
    Signed-off-by: Emrys <Emrys_02@outlook.com>

--- lib/core/routers/routers.dart
+++ lib/core/routers/routers.dart
@@ -1,5 +1,11 @@
 import 'package:defifundr_mobile/core/routers/routes_constant.dart';
+import 'package:defifundr_mobile/feature/auth_screens/screens/individual_account_flow/screens/account_type_screen.dart';
+import 'package:defifundr_mobile/feature/auth_screens/screens/individual_account_flow/screens/address_details_screen.dart';
+import 'package:defifundr_mobile/feature/auth_screens/screens/individual_account_flow/screens/personal_details_screen.dart';
 import 'package:defifundr_mobile/feature/auth_screens/screens/individual_account_flow/screens/profile_created_screen.dart';
+import 'package:defifundr_mobile/feature/auth_screens/screens/individual_account_flow/widgets/country_selection.dart';
+import 'package:defifundr_mobile/feature/auth_screens/screens/individual_account_flow/widgets/dial_code_selection.dart';
+import 'package:defifundr_mobile/feature/auth_screens/screens/login_screen/login_screen.dart';
 import 'package:defifundr_mobile/feature/auth_screens/widgets/fade_transition_page_router.dart';
 import 'package:go_router/go_router.dart';
 
@@ -12,42 +18,42 @@ class AppRouter {
         path: '/',
         name: RouteConstants.login,
         pageBuilder: (context, state) {
-          return FadeTransitionPage(key: state.pageKey, child: const ProfileCreatedScreen());
+          return FadeTransitionPage(key: state.pageKey, child: const LoginScreen());
         },
       ),
       GoRoute(
         path: '/account-type',
         name: RouteConstants.accountType,
         pageBuilder: (context, state) {
-          return FadeTransitionPage(key: state.pageKey, child: const ProfileCreatedScreen());
+          return FadeTransitionPage(key: state.pageKey, child: const AccountTypeScreen());
         },
       ),
       GoRoute(
         path: '/personal-details',
         name: RouteConstants.personalDetails,
         pageBuilder: (context, state) {
-          return FadeTransitionPage(key: state.pageKey, child: const ProfileCreatedScreen());
+          return FadeTransitionPage(key: state.pageKey, child: const PersonalDetailsScreen());
         },
       ),
       GoRoute(
         path: '/country-selection',
         name: RouteConstants.countrySelection,
         pageBuilder: (context, state) {
-          return FadeTransitionPage(key: state.pageKey, child: const ProfileCreatedScreen());
+          return FadeTransitionPage(key: state.pageKey, child: const CountrySelectionScreen());
         },
       ),
       GoRoute(
         path: '/dial-code-selection',
         name: RouteConstants.dialCodeSelection,
         pageBuilder: (context, state) {
-          return FadeTransitionPage(key: state.pageKey, child: const ProfileCreatedScreen());
+          return FadeTransitionPage(key: state.pageKey, child: const DialCodeSelectionScreen());
         },
       ),
       GoRoute(
         path: '/address-details',
         name: RouteConstants.addressDetails,
         pageBuilder: (context, state) {
-          return FadeTransitionPage(key: state.pageKey, child: const ProfileCreatedScreen());
+          return FadeTransitionPage(key: state.pageKey, child: const AddressDetailsScreen());
         },
       ),
       GoRoute(
