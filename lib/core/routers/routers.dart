329bf6f0c99918bef837528d141b10b2cba54d52
webs127 <davidshittu20@gmail.com>
2025-06-04 13:25:09 +0100

    Route ffor fundwallet and fundwallet screen

--- lib/core/routers/routers.dart
+++ lib/core/routers/routers.dart
@@ -7,6 +7,7 @@ import 'package:defifundr_mobile/feature/auth_screens/screens/individual_account
 import 'package:defifundr_mobile/feature/auth_screens/screens/individual_account_flow/widgets/dial_code_selection.dart';
 import 'package:defifundr_mobile/feature/auth_screens/screens/login_screen/login_screen.dart';
 import 'package:defifundr_mobile/feature/fundwallet_screens/fundwallet.dart';
+import 'package:defifundr_mobile/feature/fundwallet_screens/fundwallet_details.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -152,6 +153,24 @@ class AppRouter {
             return CustomTransitionPage(
               key: state.pageKey,
               child: const FundWalletScreen(),
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+                return FadeTransition(
+                  opacity: CurveTween(curve: Curves.easeInOutCirc)
+                      .animate(animation),
+                  child: child,
+                );
+              },
+            );
+          },
+        ),
+        GoRoute(
+          path: '/fund-walletdetails',
+          name: RouteConstants.fundWallet,
+          pageBuilder: (context, state) {
+            return CustomTransitionPage(
+              key: state.pageKey,
+              child: const FundWalletDetails(),
               transitionsBuilder:
                   (context, animation, secondaryAnimation, child) {
                 return FadeTransition(
